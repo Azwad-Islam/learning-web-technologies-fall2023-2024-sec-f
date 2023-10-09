da8e1e71b469b9653d8716c572137466c6166ae0
Azwad Islam <72523235+Azwad-Islam@users.noreply.github.com>
2023-10-09 17:39:50 +0600

    Add files via upload

---
+++ lab task 3/BG/BG_B/bgshow.php
@@ -0,0 +1,35 @@
+<html>
+
+<head>
+    <title>LabTask 3.1</title>
+</head>
+
+<body>
+    <form method="post">
+        <fieldset>
+            <legend>Blood Group</legend>
+            <select name="bg">
+                <option value="A+">A+</option>
+                <option value="A-">A-</option>
+                <option value="O+">O+</option>
+                <option value="O-">O-</option>
+                <option value="AB+">AB+</option>
+                <option value="AB-">AB-</option>
+                <option value="B+">B+</option>
+                <option value="B-">B-</option>
+            </select>
+            <hr>
+            <input type="submit" name="submit" value="Submit" />
+        </fieldset>
+    </form>
+</body>
+
+</html>
+
+<?php
+
+if (isset($_POST['submit'])) {
+    $bg = $_POST['bg'];
+    echo"BG: " .$bg;
+}
+?>
