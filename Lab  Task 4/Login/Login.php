90743617108166e6d935608022b0ddb8f0044f5a
Azwad Islam <azwad.sachcha@gmail.com>
2023-10-14 16:51:26 +0600

    updated

--- Lab  Task 4/Login/Login.php
+++ Lab  Task 4/Login/Login.php
@@ -1,34 +1,12 @@
-<html>
-<head>
-    <title>Lab 4 task1</title>
-</head>
-
-<body>
-    <form method="post">
-        <fieldset>
-            <legend>Login</legend>
-            User Name : <input type="text" name="UserName" value="" />
-            Password : <input type="password" name="password" value="" />
-            <hr>
-            <input type="submit" name="submit" value="Submit" />
-            <input type="checkbox" name="remember me" value=""> Remember Me<br>
-          
-            <a href="">Forgot Password?</a>
-        
-        </fieldset>
-    </form>
-</body>
-</html>
-
 <?php
 
     $symbol = array ('-','_','.',);
     
-    $PassSymbol = array('@', '#', '$', '%');
+   
     
     $alphabet = array_merge(range('a', 'z'), range('A', 'Z'), $symbol);
     global $count;
-    global $count2;
+   
 
 if (isset($_POST['submit'])) {
     $UserName = $_POST['UserName'];
@@ -44,14 +22,6 @@ if (isset($_POST['submit'])) {
         }    
     }
     
-    foreach(str_split($pass) as $char2)
-    {
-        if(!in_array($char2, $PassSymbol))
-        {
-            $count2++;
-             
-        }    
-    }
 
     if ($count > 0 ) {
         echo"Invaild Username ";
