d733fb54fa33fdd0f2c2a97553ada60796287653
Azwad Islam <azwad.sachcha@gmail.com>
2023-12-02 15:00:32 +0600

    Update adminSearch.js

--- final Lab test/asset/adminSearch.js
+++ final Lab test/asset/adminSearch.js
@@ -7,14 +7,14 @@ function ajax() {
         xhttp.onreadystatechange = function () {
 
             if (this.readyState == 4 && this.status == 200) {
-                let responseArray = this.responseText.split("+");
+                let Array = this.responseText.split("+");
 
-                let name = responseArray[0];
-                let contactNumber = responseArray[1];
-                let username = responseArray[2];
-                let password = responseArray[3];
+                let name = Array[0];
+                let contactNumber = Array[1];
+                let username = Array[2];
+                let password = Array[3];
 
-                document.getElementById('search').innerHTML = 'Name: ' + name + ',  username: ' + username ', pass: '+ password ;
+                document.getElementById('search').innerHTML = 'Name: ' + name + ',  username: ' + username ',pass: ' + password ;
             }
         }
         xhttp.send();
